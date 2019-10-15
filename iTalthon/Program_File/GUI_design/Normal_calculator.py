c0024430ced1700c12b317ccfd32047d79548d29
DD0s-JCN13 <jimmylin160@gmail.com>
2019-10-15 21:09:48 +0800

    normal calculator temp file update @ 1015/21.Oc

--- iTalthon/Program_File/GUI_design/Normal_calculator.py
+++ iTalthon/Program_File/GUI_design/Normal_calculator.py
@@ -11,7 +11,7 @@ record the input data.
 
 """
 top_number_box = Frame(main_logger)
-number_label = Label(top_number_box,text="")
+number_label = Label(top_number_box, text="")
 number_blank = Text(number_label)
 number_blank.insert(INSERT, "Testing code")
 number_blank.pack()
@@ -24,5 +24,50 @@ btn_line1 = Frame(btn_box)
 btn_line1.pack()
 btn_line2 = Frame(btn_box)
 btn_line2.pack()
+btn_line3 = Frame(btn_box)
+btn_line3.pack()
+btn_line4 = Frame(btn_box)
+btn_line4.pack()
+btn_box.pack()
+
+#for line 1
+num_btn_1 = Button(btn_line1, text="  1  ", command=None)
+num_btn_1.pack(side=LEFT)
+num_btn_2 = Button(btn_line1, text="  2  ", command=None)
+num_btn_2.pack(side=LEFT)
+num_btn_3 = Button(btn_line1, text="  3  ", command=None)
+num_btn_3.pack(side=LEFT)
+cal_btn_plu = Button(btn_line1, text="  +  ", command=None)
+cal_btn_plu.pack(side=LEFT)
+
+#for line 2
+num_btn_4 = Button(btn_line2, text="  4  ", command=None)
+num_btn_4.pack(side=LEFT)
+num_btn_5 = Button(btn_line2, text="  5  ", command=None)
+num_btn_5.pack(side=LEFT)
+num_btn_6 = Button(btn_line2, text="  6  ", command=None)
+num_btn_6.pack(side=LEFT)
+cal_btn_minus = Button(btn_line2, text="   -  ", command=None)
+cal_btn_minus.pack(side=LEFT)
+
+#for line 3
+num_btn_7 = Button(btn_line3, text="  7  ", command=None)
+num_btn_7.pack(side=LEFT)
+num_btn_8 = Button(btn_line3, text="  8  ", command=None)
+num_btn_8.pack(side=LEFT)
+num_btn_9 = Button(btn_line3, text="  9  ", command=None)
+num_btn_9.pack(side=LEFT)
+cal_btn_times = Button(btn_line3, text="  *   ", command=None)
+cal_btn_times.pack(side=LEFT)
+
+#for line 4
+cal_btn_doter = Button(btn_line4, text="   .  ", command=None)
+cal_btn_doter.pack(side=LEFT)
+num_btn_0 = Button(btn_line4, text="  0  ", command=None)
+num_btn_0.pack(side=LEFT)
+cal_btn_equ = Button(btn_line4, text="  =   ", command=None)
+cal_btn_equ.pack(side=LEFT)
+cal_btn_dvs = Button(btn_line4, text="  /  ", command=None)
+cal_btn_dvs.pack(side=LEFT)
 
 main_logger.mainloop()
